0f65481c628ac798868603070cd872b2d9eca524
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2021-08-01 10:06:35 -0300

    v0.0.4-beta

--- lib/lib/bs/src/components/Ancestor_Flex.d
+++ lib/lib/bs/src/components/Ancestor_Flex.d
@@ -1,1 +1,1 @@
-src/components/Ancestor_Flex.cmj : src/components/Ancestor_Base.cmj src/components/Ancestor_Base.cmi src/core/Ancestor_Core.cmj src/core/Ancestor_Core.cmi
+src/components/Ancestor_Flex.cmj : src/components/Ancestor_Base.cmj src/components/Ancestor_Base.cmi src/core/Ancestor_Core.cmj src/core/Ancestor_Core.cmi src/bindings/Emotion.cmj src/bindings/Emotion.cmi
